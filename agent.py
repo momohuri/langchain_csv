8c42e8a1600e3f8e35f08335cb25f14e056b3102
maurin.lenglart <momohuri>
2023-07-17 15:07:46 -0700

    adjustments to prompt + loader

--- agent.py
+++ agent.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 # Setting up the api key
 import environ
+from langchain.chat_models import ChatOpenAI
 
 env = environ.Env()
 environ.Env.read_env()
@@ -12,75 +13,52 @@ API_KEY = env("apikey")
 
 
 def create_agent(filename: str):
-    """
-    Create an agent that can access and use a large language model (LLM).
-
-    Args:
-        filename: The path to the CSV file that contains the data.
-
-    Returns:
-        An agent that can access and use the LLM.
-    """
-
     # Create an OpenAI object.
-    llm = OpenAI(openai_api_key=API_KEY)
+    llm = ChatOpenAI(openai_api_key=API_KEY, model='gpt-4', temperature=0.5)
 
     # Read the CSV file into a Pandas DataFrame.
     df = pd.read_csv(filename)
 
     # Create a Pandas DataFrame agent.
-    return create_pandas_dataframe_agent(llm, df, verbose=False)
+    return create_pandas_dataframe_agent(llm, df, verbose=True)
 
 
 def query_agent(agent, query):
-    """
-    Query an agent and return the response as a string.
-
-    Args:
-        agent: The agent to query.
-        query: The query to ask the agent.
-
-    Returns:
-        The response from the agent as a string.
-    """
-
     prompt = (
-        """
-            For the following query, if it requires drawing a table, reply as follows:
-            {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
-
-            If the query requires creating a bar chart, reply as follows:
-            {"bar": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
-            
-            If the query requires creating a line chart, reply as follows:
-            {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
-            
-            There can only be two types of chart, "bar" and "line".
-            
-            If it is just asking a question that requires neither, reply as follows:
-            {"answer": "answer"}
-            Example:
-            {"answer": "The title with the highest rating is 'Gilead'"}
-            
-            If you do not know the answer, reply as follows:
-            {"answer": "I do not know."}
-            
-            Return all output as a string.
-            
-            All strings in "columns" list and data list, should be in double quotes,
-            
-            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
-            
-            Lets think step by step.
-            
-            Below is the query.
-            Query: 
             """
-        + query
+                SYSTEM:
+                For the human query, if it requires drawing a table, reply as follows:
+                {"table": {"columns": ["column1", "column2", ...], "data": [[value1, value2, ...], [value1, value2, ...], ...]}}
+    
+                If the query requires creating a bar chart, reply as follows:
+                {"bar": {"columns": ["label1", "label2", "label3", ...], "data": [10,2,30, ...]}}
+                
+                If the query requires creating a line chart, reply as follows:
+                {"line": {"columns": ["A", "B", "C", ...], "data": [25, 24, 10, ...]}}
+                The length of columns should be equal to the length of data
+                
+                There can only be two types of chart, "bar" and "line".
+                
+                If it is just asking a question that requires neither, reply as follows:
+                {"answer": "answer"}
+                Example:
+                {"answer": "The title with the highest rating is 'Gilead'"}
+                
+                If you do not know the answer, reply as follows:
+                {"answer": "I do not know."}
+                
+                All strings in "columns" list and data list, should be in double quotes,
+                
+                For example: {"columns": ["industry1", "industry2"], "data": [100, 4000]}
+                
+                ------------
+                Human: 
+                """
+            + query
     )
 
     # Run the prompt through the agent.
     response = agent.run(prompt)
-
+    print(response)
     # Convert the response to a string.
     return response.__str__()
--- interface.py
+++ interface.py
@@ -6,28 +6,10 @@ from agent import query_agent, create_agent
 
 
 def decode_response(response: str) -> dict:
-    """This function converts the string response from the model to a dictionary object.
-
-    Args:
-        response (str): response from the model
-
-    Returns:
-        dict: dictionary with response data
-    """
     return json.loads(response)
 
 
 def write_response(response_dict: dict):
-    """
-    Write a response from an agent to a Streamlit app.
-
-    Args:
-        response_dict: The response from the agent.
-
-    Returns:
-        None.
-    """
-
     # Check if the response is an answer.
     if "answer" in response_dict:
         st.write(response_dict["answer"])
@@ -62,14 +44,15 @@ data = st.file_uploader("Upload a CSV")
 query = st.text_area("Insert your query")
 
 if st.button("Submit Query", type="primary"):
-    # Create an agent from the CSV file.
-    agent = create_agent(data)
+    with st.spinner(text="In progress..."):
+        # Create an agent from the CSV file.
+        agent = create_agent(data)
 
-    # Query the agent.
-    response = query_agent(agent=agent, query=query)
+        # Query the agent.
+        response = query_agent(agent=agent, query=query)
 
-    # Decode the response.
-    decoded_response = decode_response(response)
+        # Decode the response.
+        decoded_response = decode_response(response)
 
-    # Write the response to the Streamlit app.
+        # Write the response to the Streamlit app.
     write_response(decoded_response)
